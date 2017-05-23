00e7016cb565a7aa54db0cc901d6fe513507bad4
German Munoz <germanmunozbravo@gmail.com>
2017-05-22 23:45:46 -0500

    Se adiciona .gitignore y recog_watson.py cambia para hacer reconocimiento de a chunks of buffer_size

--- banrep/recog_watson.py
+++ banrep/recog_watson.py
@@ -1,22 +1,66 @@
 # coding: utf-8
-"""Modulo para extraer texto de archivos binarios."""
-import csv
+"""Modulo para speech-2-text a un archivo de audio usando IBM Watson. """
+from mimetypes import MimeTypes
 import datetime
 import json
 import logging
 import os
 import sys
 import time
-import warnings
 
 from watson_developer_cloud import SpeechToTextV1
 
+
+def guess_mime_type(audio_path, forced_mime=None):
+    mime = MimeTypes()
+    mime_type = forced_mime or mime.guess_type(audio_path)[0]
+    logstr = 'MimeType de archivo {}: {}'.format(audio_path, mime_type)
+    logging.info(logstr)
+
+    return mime_type
+
+
+def chunked_upload(audio_path, buffer_size):
+    total_size = os.path.getsize(audio_path)
+    progress = 0
+
+    with open(audio_path, 'rb') as audio_file:
+        while True:
+            data = audio_file.read(buffer_size)
+            progress += len(data or [])
+            logstr = 'Leidos {} bytes de {}'.format(progress, total_size)
+            logging.info(logstr)
+
+            if not data:
+                break
+            yield data
+
+
+def recognize_speech(username, password, audio_path,
+                     buffer_size=4096, **kwargs):
+    stt = SpeechToTextV1(username=username, password=password)
+
+    return stt.recognize(
+        chunked_upload(audio_path, buffer_size), **kwargs)
 
 
 def main():
     """Unificar en main para poder ejecutar despues desde otro script."""
     inicio = time.time()
-    corrida = "{:%Y-%m-%d-%H%M%S}".format(datetime.datetime.now())
+
+    dir_logs = 'logs'
+    os.makedirs(dir_logs, exist_ok=True)
+
+    ahora = datetime.datetime.now()
+    corrida = "{:%Y-%m-%d-%H%M%S}".format(ahora)
+    logfile = os.path.join(dir_logs, '{}.log'.format(corrida))
+    log_format = '%(asctime)s : %(levelname)s : %(message)s'
+    log_datefmt = '%Y-%m-%d %H:%M:%S'
+    logging.basicConfig(format=log_format,
+                        datefmt=log_datefmt,
+                        level=logging.INFO,
+                        filename=logfile,
+                        filemode='w')
 
     dir_input = sys.argv[1]
     credfile = sys.argv[2]
@@ -28,17 +72,25 @@ def main():
     username = credential.get('username')
     password = credential.get('password')
 
-    stt = SpeechToTextV1(username=username, password=password)
-
     audiofile = os.path.join(dir_input, 'prueba.flac')
 
-    with open(audiofile, 'rb') as af:
-        result = stt.recognize(af, content_type="audio/x-flac",
-                               timestamps=True, max_alternatives=1,
-                               model='es-ES_BroadbandModel')
+    specifics = {
+        'content_type': guess_mime_type(audiofile, 'audio/x-flac'),
+        'timestamps': True,
+        'speaker_labels': True,
+        'max_alternatives': 1,
+        'model': 'es-ES_BroadbandModel'
+    }
 
     with open('transcript_result.json', 'w', encoding='utf-8') as fp:
+        result = recognize_speech(username=username, password=password,
+                                  audio_path=audiofile, **specifics)
+
         json.dump(result, fp, indent=2, ensure_ascii=False)
+
+    time_end = time.time()
+    secs = time_end - inicio
+    logging.info('{m:.2f} minutos'.format(m=secs / 60))
 
 
 if __name__ == '__main__':
